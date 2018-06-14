bbdc9a66a147c218a0ed92c582fd2130d76e2751
baby-blue <zhenxing04@21cn.com>
2018-06-14 23:20:07 +0800

    recreate main

--- dr_yi/main.aspx.cs
+++ dr_yi/main.aspx.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace analyzer
+namespace dr_yi
 {
     public partial class main : System.Web.UI.Page
     {
